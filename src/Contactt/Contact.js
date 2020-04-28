06a55245fe2b36f2d983e8c8dcbf0726bdcc238f
moshe vilner <shootermv@gmail.com>
2020-04-28 15:27:10 +0300

    improve styles of contact

--- src/Contactt/Contact.js
+++ src/Contactt/Contact.js
@@ -5,7 +5,9 @@ const Contact = ({contact}) => {
   const {name, driverType, driverRank, email, phone, profile_image: img } = contact;
   return (
     <article className="card">
-       <img src={img}/>
+       <div className="img-wrap">
+         <img src={img}/>
+       </div>
 
        <div className="details-container">
           <h3>{name}</h3>
