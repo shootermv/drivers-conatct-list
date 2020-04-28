aba0a7562cd5316c0f089563beec14be898a65c4
moshe vilner <shootermv@gmail.com>
2020-04-28 18:18:42 +0300

    Updates

---
+++ precache-manifest.aee5091ce32f23d609f2b0a709383714.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "488225d385a57925192bea67b20df7fa",
+    "url": "/drivers-contact-list/index.html"
+  },
+  {
+    "revision": "ab834776aa2d98f6e1d9",
+    "url": "/drivers-contact-list/static/css/main.7746296f.chunk.css"
+  },
+  {
+    "revision": "763d62e9e393eafbeaa6",
+    "url": "/drivers-contact-list/static/js/2.91412aba.chunk.js"
+  },
+  {
+    "revision": "ab834776aa2d98f6e1d9",
+    "url": "/drivers-contact-list/static/js/main.3120fd9f.chunk.js"
+  },
+  {
+    "revision": "4fee6b9f6d3c6fd768e6",
+    "url": "/drivers-contact-list/static/js/runtime~main.2a74e62a.js"
+  },
+  {
+    "revision": "61d9d7c44697792cbfdf770998eb0347",
+    "url": "/drivers-contact-list/static/media/citizen.61d9d7c4.svg"
+  },
+  {
+    "revision": "306efb7cfdd2730fd2b5f8cc93af6002",
+    "url": "/drivers-contact-list/static/media/professional.306efb7c.svg"
+  },
+  {
+    "revision": "f63792ab1bac89098b53e22a0a07f61b",
+    "url": "/drivers-contact-list/static/media/searchIcon.f63792ab.svg"
+  }
+]);
