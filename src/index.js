a65d2075db7c89f220a6ea18ca26fd30a06dc3ad
moshe vilner <shootermv@gmail.com>
2020-04-29 16:13:48 +0300

    store provider attached to app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 import App from "./App";
+
+import reducers from "./store/reducers";
+
+
+const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   rootElement
 );
