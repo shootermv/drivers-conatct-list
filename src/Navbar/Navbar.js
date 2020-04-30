9d9d66e90507fd1f15f172bbe3f5690470ca9b0d
moshe vilner <shootermv@gmail.com>
2020-04-30 10:17:22 +0300

    fix(warning): replace onInput handler by onChange

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -12,7 +12,8 @@ import {Input, Form, Brand, Header} from  './Navbar.styles.js';
 
 
 const Navbar = () => {
-  const term = useSelector(({term}) => term);
+  const term = useSelector(({term}) => term || '');
+  const chang = e => dispatch(filterContacts(e.target.value));
   const dispatch = useDispatch();
   return (
     <Header>
@@ -23,7 +24,7 @@ const Navbar = () => {
             </a>
         </Brand>
         <Form>
-            <Input placeholder="search" value={term} onInput={e => dispatch(filterContacts(e.target.value))}/>
+            <Input placeholder="search" value={term} onChange={chang}/>
             {term ? <img alt={'clean'} src={cleanBtn} onClick={e => dispatch(filterCleaned())}/>: <img  alt={'search'} src={searchIcon}/>}
         </Form>
     </Header>
