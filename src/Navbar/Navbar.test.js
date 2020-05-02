efab0e0f94cd941fa5de911ba6fb499d560ec76a
moshe vilner <shootermv@gmail.com>
2020-05-02 21:06:28 +0300

    adding test to Navbar component

--- src/Navbar/Navbar.test.js
+++ src/Navbar/Navbar.test.js
@@ -7,14 +7,29 @@ import reducer from '../store/reducers';
 import Navbar from './Navbar'
 
 describe('Navbar spec', () => {
-    const mockStore = createStore(reducer, {term: null, });
+    
 
     it('renders the component', () => {
+      const mockStore = createStore(reducer, {term: null});  
       const container = render(
         <Provider store={mockStore}>
            <Navbar/>
         </Provider>
       )
       expect(container.firstChild).toMatchSnapshot()
-     })
+    })
+
+    it('should call "filterContacts" action  when somebody types at the input', () => {
+        const mockStore = createStore(reducer, {term: null});  
+        mockStore.dispatch = jest.fn();
+        render(
+          <Provider store={mockStore}>
+             <Navbar/>
+          </Provider>
+        )
+        const inpt = document.querySelector('input');
+        const target = {target: {value: '1'}};
+        fireEvent.change(inpt, target);
+        expect(mockStore.dispatch).toHaveBeenCalledTimes(1);
+    })     
 })    
