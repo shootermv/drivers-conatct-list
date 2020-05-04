72aef461b2daec1f2a293d087bf551f3b0aef15c
moshe vilner <shootermv@gmail.com>
2020-05-04 09:06:00 +0300

    loader component logic moved to Loader file

---
+++ src/shared/Loader/Loader.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {LoaderWrap, Spinner} from './Loader.styles'
+export default () => (
+    <LoaderWrap>
+       <Spinner/>
+    </LoaderWrap>
+)
--- src/shared/Loader/index.js
+++ src/shared/Loader/index.js
@@ -1,7 +1,2 @@
-import React from 'react';
-import {LoaderWrap, Spinner} from './index.styles'
-export default () => (
-    <LoaderWrap>
-       <Spinner/>
-    </LoaderWrap>
-)
+export {default} from './Loader'
+    
