ff9e1a7e48275038279a8d4d7cacdd3ec5cc81d6
moshe vilner <shootermv@gmail.com>
2020-05-07 08:18:53 +0300

    added 'dark' and 'light' stories for themeToggle

---
+++ src/Navbar/ThemeToggle/ThemeToggle.stories.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { ThemeProvider } from 'styled-components';
+
+import ThemeToggle from './ThemeToggle';
+export default {
+    title: 'ThemeToggle',
+    component: ThemeToggle,
+};
+
+
+export const ThemeToggleStoryLight = () => (
+    <Provider store={createStore(()=>({theme: 'light'}))}>
+      <ThemeProvider theme={{nm: 'light'}}>
+         <ThemeToggle />
+      </ThemeProvider>     
+    </Provider>
+);
+
+export const ThemeToggleStoryDark = () => (
+    <Provider store={createStore(()=>({theme: 'dark'}))}>
+      <ThemeProvider theme={{nm: 'dark'}}>
+         <ThemeToggle />
+      </ThemeProvider>     
+    </Provider>
+);
