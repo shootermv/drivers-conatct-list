4a871866401d1497b23556f213d3d0a4e5cd7c67
moshe vilner <shootermv@gmail.com>
2021-05-02 13:33:46 +0300

    refactor: ⛳Navbar should use 'getTerm' selector

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,35 +1,50 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import {filterChanged} from '../store/actions';
 
 /* ICONS */
-import searchIcon from './searchIcon.svg';
-import logo from './logo.svg';
-import cleanBtn from './cleanBtn.svg';
-import ThemeToggle from './ThemeToggle'
+import searchIcon from "./searchIcon.svg";
+import logo from "./logo.svg";
+import cleanBtn from "./cleanBtn.svg";
+import ThemeToggle from "./ThemeToggle";
+/* redux stuff */
+import { useSelector, useDispatch } from "react-redux";
+import { filterChanged } from "../store/actions";
+import { getTerm } from "../store/selectors";
 
-import {Input, Form, Brand, Header} from  './Navbar.styles.js';
-
+import { Input, Form, Brand, Header } from "./Navbar.styles.js";
 
 const Navbar = () => {
-  const term = useSelector(({contact: {term}}) => term || '');
-  const changeHandler = e => dispatch(filterChanged(e.target.value));
+  const term = useSelector(getTerm);
+  const changeHandler = (e) => dispatch(filterChanged(e.target.value));
   const dispatch = useDispatch();
   return (
     <Header>
-        <Brand>
-            <a href="/" data-testid="brand">
-                <span>Contact List</span>
-                <img src={logo} alt={'company logo'}/>
-            </a>
-        </Brand>
-        <ThemeToggle/>
-        <Form>
-            <Input placeholder="search" value={term} onChange={changeHandler} data-testid="search"/>
-            {term ? <img alt={'clean'} src={cleanBtn} onClick={e => dispatch(filterChanged(''))}/>: <img  alt={'search'} src={searchIcon}/>}
-        </Form>
+      <Brand>
+        <a href="/" data-testid="brand">
+          <span>Contact List</span>
+          <img src={logo} alt={"company logo"} />
+        </a>
+      </Brand>
+      <ThemeToggle />
+      <Form>
+        <Input
+          placeholder="search"
+          value={term}
+          onChange={changeHandler}
+          data-testid="search"
+        />
+        {term ? (
+          <img
+            alt={"clean"}
+            src={cleanBtn}
+            data-testid="clean-filter"
+            onClick={(e) => dispatch(filterChanged(""))}
+          />
+        ) : (
+          <img alt={"search"} src={searchIcon} />
+        )}
+      </Form>
     </Header>
-   );
+  );
 };
 
 export default Navbar;
