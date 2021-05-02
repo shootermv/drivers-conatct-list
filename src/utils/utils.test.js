28d3a73d3cd3cde4176349d74db577dbf564ff79
moshe vilner <shootermv@gmail.com>
2021-05-02 09:55:26 +0300

    test: added test for shallow copy

--- src/utils/utils.test.js
+++ src/utils/utils.test.js
@@ -1,51 +1,75 @@
+import { searchByText, formatPhone } from "./index";
 
-import {searchByText, formatPhone} from './index';
-
-describe('utils formatPhone', () => {
-
-  it('formatPhone - should add mmissing hyphens', () => {
-    const term = '1234567890';
-    const result = '123-456-7890';
+describe("utils formatPhone", () => {
+  it("formatPhone - should add mmissing hyphens", () => {
+    const term = "1234567890";
+    const result = "123-456-7890";
     expect(formatPhone(term)).toEqual(result);
   });
 
-  it('formatPhone - should replace dots by hyphens', () => {
-    const term = '123.456.7890';
-    const result = '123-456-7890';
+  it("formatPhone - should replace dots by hyphens", () => {
+    const term = "123.456.7890";
+    const result = "123-456-7890";
     expect(formatPhone(term)).toEqual(result);
   });
 
-  it('formatPhone - should replace spaces by hyphens', () => {
-    const term = '123 456   7890';
-    const result = '123-456-7890';
+  it("formatPhone - should replace spaces by hyphens", () => {
+    const term = "123 456   7890";
+    const result = "123-456-7890";
     expect(formatPhone(term)).toEqual(result);
   });
-})
+});
 
+describe("utils searchByText method", () => {
+  it("searchByText - case should be ignored", () => {
+    const arr = [
+      {
+        name: "Vasia",
+      },
+      {
+        name: "Petia",
+      },
+    ];
+    const term = "v";
+    expect(searchByText(arr, term)).toEqual([
+      {
+        name: "Vasia",
+      },
+    ]);
+  });
 
-describe('utils searchByText', () => {
-    it('searchByText - case should be ignored', () => {
-      const arr = [{
-          name: 'Vasia',
-      },{
-        name: 'Petia',
-      }];
-      const term = 'v'
-      expect(searchByText(arr, term)).toEqual([{
-        name: 'Vasia',
-      }]);
-    });
+  it('searchByText - "excluded" field should be ignored', () => {
+    const arr = [
+      {
+        name: "Vasia",
+        ex: "",
+      },
+      {
+        name: "Petia",
+        ex: "vvv",
+      },
+    ];
+    const term = "vvv";
+    const excluded = "ex";
+    expect(searchByText(arr, term, excluded)).toEqual([]);
+  });
 
-    it('searchByText - "excluded" field should be ignored', () => {
-        const arr = [{
-            name: 'Vasia',
-            ex: ''
-        },{
-            name: 'Petia',
-            ex: 'vvv'
-        }];
-        const term = 'vvv'
-        const excluded = 'ex'
-        expect(searchByText(arr, term, excluded)).toEqual([]);
-    });
-})
+  it('searchByText - when term is empty - the whole array should return', () => {
+    const arr = [
+      {
+        name: "Vasia",
+        ex: "",
+      },
+      {
+        name: "Petia",
+        ex: "vvv",
+      },
+    ];
+    const term = "";
+    const excluded = "ex";
+    const resultArr = searchByText(arr, term, excluded)
+    expect(arr).toEqual(resultArr);
+    expect(arr).not.toBe(resultArr); // should be shallow copy of array
+  });
+
+});
