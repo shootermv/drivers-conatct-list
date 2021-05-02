4ed7a09159f48a99ccbb004c385105914978e365
moshe vilner <shootermv@gmail.com>
2021-05-02 09:55:08 +0300

    refactor: reducers file formmated

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,57 +1,58 @@
-import { contactConstants } from './constants';
-import { searchByText } from '../utils';
+import { contactConstants } from "./constants";
+import { searchByText } from "../utils";
 import { combineReducers } from "redux";
 
-export function contact(state = {contacts:[], filtered:[], term: null, loading: false}, action) {
-   
-    switch (action.type) {
+export function contact(
+  state = { contacts: [], filtered: [], term: null, loading: false },
+  action
+) {
+  switch (action.type) {
+    case contactConstants.REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
 
-        case contactConstants.REQUEST:  
-            return  {     
-                ...state,           
-                loading: true 
-            };
+    case contactConstants.SUCCESS:
+      const { contacts } = action;
+      return {
+        ...state,
+        contacts,
+        filtered: contacts,
+        loading: false,
+      };
 
-        case contactConstants.SUCCESS:       
-           const {contacts} =  action;
-            return  {     
-                ...state,           
-                contacts,
-                filtered: contacts,
-                loading: false
-            };
-            
-        case contactConstants.FILTERED:  
-            let {term} = action;
-            let filtered = searchByText(state.contacts, term, 'profile_image')
-            return {                
-                ...state,
-                term,
-                filtered
-            };
+    case contactConstants.FILTERED:
+      let { term } = action;
+      let filtered = searchByText(state.contacts, term, "profile_image");
+      return {
+        ...state,
+        term,
+        filtered,
+      };
 
-        case contactConstants.FILTER_CLEANED:
-            return {    
-                ...state,
-                term: '',
-                filtered: state.contacts
-            };    
-    
-        default:
-            return state
-    }
-} 
+    case contactConstants.FILTER_CLEANED:
+      return {
+        ...state,
+        term: "",
+        filtered: state.contacts,
+      };
 
-export function theme (state = 'light', action) {
-    switch (action.type) {
-      case 'THEME':
-        return action.theme   
-      default:
-        return state
-    }
-} 
+    default:
+      return state;
+  }
+}
+
+export function theme(state = "light", action) {
+  switch (action.type) {
+    case "THEME":
+      return action.theme;
+    default:
+      return state;
+  }
+}
 
 export default combineReducers({
-   contact,
-   theme
+  contact,
+  theme,
 });
