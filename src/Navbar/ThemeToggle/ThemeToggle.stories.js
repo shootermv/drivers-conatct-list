893c300c02a6fd69ef23e3d30f6ac9ff2f4ed890
moshe vilner <shootermv@gmail.com>
2021-05-03 06:15:28 +0300

    test(story): theme story file formatted

--- src/Navbar/ThemeToggle/ThemeToggle.stories.js
+++ src/Navbar/ThemeToggle/ThemeToggle.stories.js
@@ -1,27 +1,26 @@
-import React from 'react';
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import { ThemeProvider } from 'styled-components';
+import React from "react";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { ThemeProvider } from "styled-components";
 
-import ThemeToggle from './ThemeToggle';
+import ThemeToggle from "./ThemeToggle";
 export default {
-    title: 'ThemeToggle',
-    component: ThemeToggle,
+  title: "ThemeToggle",
+  component: ThemeToggle,
 };
 
-
 export const ThemeToggleStoryLight = () => (
-    <Provider store={createStore(()=>({theme: 'light'}))}>
-      <ThemeProvider theme={{nm: 'light'}}>
-         <ThemeToggle />
-      </ThemeProvider>     
-    </Provider>
+  <Provider store={createStore(() => ({ theme: "light" }))}>
+    <ThemeProvider theme={{ nm: "light" }}>
+      <ThemeToggle />
+    </ThemeProvider>
+  </Provider>
 );
 
 export const ThemeToggleStoryDark = () => (
-    <Provider store={createStore(()=>({theme: 'dark'}))}>
-      <ThemeProvider theme={{nm: 'dark'}}>
-         <ThemeToggle />
-      </ThemeProvider>     
-    </Provider>
+  <Provider store={createStore(() => ({ theme: "dark" }))}>
+    <ThemeProvider theme={{ nm: "dark" }}>
+      <ThemeToggle />
+    </ThemeProvider>
+  </Provider>
 );
