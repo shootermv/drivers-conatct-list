e10c4080a6f83b417993e318789dc3bb3340908a
moshe vilner <shootermv@gmail.com>
2021-05-07 15:47:14 +0300

    feat: created getContacById selector

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -4,6 +4,7 @@ import { searchByText } from "../utils";
 export const getTerm = state => state.contact.term;
 export const getContacts = state => state.contact.contacts;
 export const getLoading = state => state.contact.loading;
+export const getContactById = (state, id) => state.contact.contacts.find(c => c.id === id);
 export const getTheme = state => state.theme;
 
 export const getVisibleContacts = createSelector(
