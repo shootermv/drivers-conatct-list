6a696a96dd4b01107dff7e82bb34f84b3eecb62f
moshe vilner <shootermv@gmail.com>
2021-05-13 15:21:37 +0300

    refactor: contact uses selector getContactbyId

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,41 +1,59 @@
 import React from "react";
-import  {Article, Imgwrap, DriverType, DetailsContainer, Img, Row} from './Contact.styles';
+import {
+  Article,
+  Imgwrap,
+  DriverType,
+  DetailsContainer,
+  Img,
+  Row,
+} from "./Contact.styles";
 
 /* IMG */
-import citizen from './citizen.svg';
-import missing from './missing.png';
-import professional from './professional.svg';
+import citizen from "./citizen.svg";
+import missing from "./missing.png";
+import professional from "./professional.svg";
+import { useSelector } from "react-redux";
+import Rating from "../shared/Rating";
 
-import Rating from '../shared/Rating';
+import { formatPhone } from "../utils";
+import { getContactById } from "../store/selectors";
 
-import {formatPhone} from '../utils'
-
-const Contact = ({contact}) => {
-  const addDefaultSrc = ev => {
+const Contact = ({ contact }) => {
+  const addDefaultSrc = (ev) => {
     ev.target.src = missing;
-  }
-  const {name, driverType, driverRank, email, phone, profile_image: img } = contact;
+  };
+  const {
+    name,
+    driverType,
+    driverRank,
+    email,
+    phone,
+    profile_image: img,
+  } = useSelector((state) => getContactById(state, contact.id));
   return (
     <Article>
-       <Imgwrap>
-         <Img src={img || missing} onError={addDefaultSrc}/>
-       </Imgwrap>
-       
-       <DetailsContainer>
-          <DriverType alt="driver type" src={driverType === 'Professional' ? professional : citizen} /> 
-          <h3>{name}</h3>
-          <div>
-            <Rating rating={driverRank}/>
-          </div>
-          <Row>
-            <label>Phone:</label> 
-            <b>{formatPhone(phone) || 'N/A'}</b>
-          </Row>
-          <Row>
-            <label>Email:</label> 
-            <b title={email}>{email || 'N/A'}</b>
-          </Row>
-       </DetailsContainer>
+      <Imgwrap>
+        <Img src={img || missing} onError={addDefaultSrc} />
+      </Imgwrap>
+
+      <DetailsContainer>
+        <DriverType
+          alt="driver type"
+          src={driverType === "Professional" ? professional : citizen}
+        />
+        <h3>{name}</h3>
+        <div>
+          <Rating rating={driverRank} />
+        </div>
+        <Row>
+          <label>Phone:</label>
+          <b>{formatPhone(phone) || "N/A"}</b>
+        </Row>
+        <Row>
+          <label>Email:</label>
+          <b title={email}>{email || "N/A"}</b>
+        </Row>
+      </DetailsContainer>
     </Article>
   );
 };
--- src/Contact/Contact.test.js
+++ src/Contact/Contact.test.js
@@ -1,42 +1,85 @@
-import React from 'react'
-import { render, fireEvent } from '@testing-library/react';
-import Contact from './Contact'
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import Contact from "./Contact";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "../store/reducers";
 
-describe('Contact spec', () => {
-    it('renders the component', () => {
-      const contact = {
-        name: '',
-        email: '', 
-        phone: '', 
-        'profile-image': '',
-        driverRank: 4
-      }  
-      const container = render(<Contact contact={contact}/>)
-      expect(container.firstChild).toMatchSnapshot()
-     })
+
+describe("Contact spec", () => {
+  it("renders the component", () => {
+    const contact = {
+      id: 1,
+      name: "",
+      email: "",
+      phone: "",
+      "profile-image": "",
+      driverRank: 4,
+    };
+    const mockStore = createStore(reducer, {
+      contact: {
+        term: "",
+        contacts: [contact],
+      },
+      theme: "",
+    });
+    const container = render(
+      <Provider store={mockStore}>
+        <Contact contact={contact} />
+      </Provider>
+    );
+    expect(container.firstChild).toMatchSnapshot();
+  });
 
   it('by default should display "sitizen" image unless contact has "driver" type', () => {
     const contact = {
-      name: '',
-      email: '', 
-      phone: '', 
-      'profile-image': '',
-      driverRank: 4
-    }  
-    const {getByAltText} = render(<Contact contact={contact}/>)
-    expect(getByAltText('driver type')).toHaveAttribute('src', 'citizen.svg')
-   })
+      id: 1,
+      name: "",
+      email: "",
+      phone: "",
+      "profile-image": "",
+      driverRank: 4,
+    };
+    const mockStore = createStore(reducer, {
+      contact: {
+        term: "",
+        contacts: [contact],
+      },
+      theme: "",
+    });
+    const { getByAltText } = render(
+      <Provider store={mockStore}>
+        <Contact contact={contact} />
+      </Provider>
+    );
+    expect(getByAltText("driver type")).toHaveAttribute("src", "citizen.svg");
+  });
 
-   it('should display "professional.svg" image if driver type is "Professional"', () => {
+  it('should display "professional.svg" image if driver type is "Professional"', () => {
     const contact = {
-      name: '',
-      email: '', 
-      phone: '', 
-      'profile-image': '',
-      driverType: 'Professional',
-      driverRank: 4
-    }  
-    const {getByAltText} = render(<Contact contact={contact}/>)
-    expect(getByAltText('driver type')).toHaveAttribute('src', 'professional.svg')
-   })   
-})  
+      id: 1,
+      name: "",
+      email: "",
+      phone: "",
+      "profile-image": "",
+      driverType: "Professional",
+      driverRank: 4,
+    };
+    const mockStore = createStore(reducer, {
+      contact: {
+        term: "",
+        contacts: [contact],
+      },
+      theme: "",
+    });
+    const { getByAltText } = render(
+      <Provider store={mockStore}>
+        <Contact contact={contact} />
+      </Provider>
+    );
+    expect(getByAltText("driver type")).toHaveAttribute(
+      "src",
+      "professional.svg"
+    );
+  });
+});
